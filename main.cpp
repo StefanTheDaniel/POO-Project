b437081f2529fc596a0728261f684d252c5c7765
StefanTheDaniel <95691101+StefanTheDaniel@users.noreply.github.com>
2025-11-03 06:09:53 +0200

    Update main.cpp
    Cod pentru M1

--- main.cpp
+++ main.cpp
@@ -1,50 +1,202 @@
+#include <algorithm>
 #include <iostream>
-#include <array>
+#include <string>
+#include <vector>
+#include <random>
+#include <ctime>
+#include <algorithm>
 
-int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+class Card
+{
+    int suit;
+    int value;
+public:
+    Card(int suit, int value) : suit(suit), value(value) {}
+
+    void print() const
+    {
+        if (suit == 4) //for the jokers
+        {
+            std::cout<<(value == 1 ? "Black Joker" : "Red Joker")<<std::endl;
+        }
+        else //for the royal cards, ace is treated as value=1
+        {
+            std::string SpecialValue;
+            switch(value)
+            {
+            case 1:SpecialValue="Ace"; break;
+            case 11:SpecialValue="Jack"; break;
+            case 12:SpecialValue="Queen"; break;
+            case 13:SpecialValue="King"; break;
+            default:SpecialValue=std::to_string(value); break;
+            }
+            std::string suitString;
+            switch (suit)
+            {
+            case 0: suitString = "Hearts";break;
+            case 1: suitString = "Spades";break;
+            case 2: suitString = "Diamonds";break;
+            case 3: suitString = "Clubs";break;
+            default: suitString = "Other Suit"; break;
+            }
+            std::cout << SpecialValue<<" of "<<suitString<<std::endl;
+        }
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
+    int getValue() const{return value;}
+    int getSuit() const{return suit;}
+};
+
+class Hand
+{
+    std::vector<Card> cards;
+    public:
+    void addCard(const Card& card)
+    {
+        cards.push_back(card);
     }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
+    void removeCard(const Card& card)
+    {
+        for(auto it=cards.begin(); it!=cards.end(); it++)
+        {
+            if(it->getSuit()==card.getSuit() && it->getValue()==card.getValue())
+            {
+                cards.erase(it);
+                break;
+            }
+        }
+    }
+    void printCards() const
+    {
+        for(const auto& card : cards)
+        {
+            card.print();
+        }
+    }
+    int getHandSize() const
+    {
+        return cards.size();
+    }
+};
+
+class Deck
+{
+    std::vector<Card> cards;
+public:
+    Deck()
+    {
+        int i=0;
+        for(int suit=0;suit<4;suit++)
+        {
+            for(int value=1; value<=13; value++)
+            {
+                cards.push_back(Card(suit, value));
+            }
+        }
+        cards.push_back(Card(4, 1)); // Black Joker
+        cards.push_back(Card(4, 2)); // Red Joker
+    }
+
+    void printDeck() const
+    {
+        for (const auto& card : cards)
+            card.print();
+    }
+
+    std::vector<Card> getDeck() const
+    {
+        return cards;
+    }
+};
+
+class Player
+{
+    std::string name;
+    int score;
+    Hand hand; //o mana pt jucator
+public:
+    Player(const std::string name) : name(name), score(0) {}
+
+    void addCardHand(const Card& card)
+    {
+        hand.addCard(card);
+    }
+    void removeCardHand(const Card& card)
+    {
+        hand.removeCard(card);
+    }
+    void printHand() const
+    {
+        std::cout<<"Hand of "<<name<<std::endl;
+        hand.printCards();
+    }
+    void addScore(int score)
+    {
+        score += score;
+    }
+    int getScore() const
+    {
+        return score;
+    }
+    const std::string getName() const
+    {
+        return name;
+    }
+};
+
+class Shuffle
+{
+    std::vector<Card> deck;
+    public:
+    Shuffle(const Deck& d)
+    {
+        deck=d.getDeck();
+    }
+    void shuffle()
+    {
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::shuffle(deck.begin(), deck.end(),g);
+    }
+
+    void dealCards(Player &player1, Player &player2, Player &player3, Player &player4)
+    {
+        int i = 0;
+        while (i < 52) // Deal 13 cards to each player, total of 52 cards in a standard deck
+        {
+            if (i < 13) {
+                player1.addCardHand(deck[i]);
+            } else if (i < 26) {
+                player2.addCardHand(deck[i]);
+            } else if (i < 39) {
+                player3.addCardHand(deck[i]);
+            } else {
+                player4.addCardHand(deck[i]);
+            }
+            i++;
+        }
+    }
+};
+
+
+
+int main()
+{
+    Deck D1;
+
+    Shuffle shuffle(D1);
+    shuffle.shuffle();
+
+    Player player1("Player 1");
+    Player player2("Player 2");
+    Player player3("Player 3");
+    Player player4("Player 4");
+
+    shuffle.dealCards(player1, player2, player3, player4);
+
+    player1.printHand();
+    player2.printHand();
+    player3.printHand();
+    player4.printHand();
+
     return 0;
 }
